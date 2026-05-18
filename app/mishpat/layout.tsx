64bd8c6778f97f2d3e3baf8ad36ba0f1ea0d1d61
Hanna <chanush2@gmail.com>
2026-05-18 13:30:31 +0300

    add google fonts

--- app/mishpat/layout.tsx
+++ app/mishpat/layout.tsx
@@ -1,3 +1,10 @@
 export default function MishpatLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+  return (
+    <>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Hebrew:wght@400;500;600&family=Rubik:wght@400;500;600&family=Figtree:wght@400;500;600&display=swap" rel="stylesheet" />
+      {children}
+    </>
+  );
 }
